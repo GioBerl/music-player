fd8ff1dd0a9bff7348dd28c7b2097be60b7d027a
GioBerl <giovanni.berl1@gmail.com>
2020-11-24 23:25:05 +0100

    cambio canzone, da risolvere play pause

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,14 +1,21 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-function Library({ songs }) {
+function Library({ songs, setCurrentSong }) {
     return (
         <div>
             <div className="library">
                 <h2>Library</h2>
                 <div className="library-songs">
                     {songs.map((song) => {
-                        return <LibrarySong song={song} />;
+                        return (
+                            <LibrarySong
+                                key={song.id}
+                                song={song}
+                                setCurrentSong={setCurrentSong}
+                                songs={songs}
+                            />
+                        );
                     })}
                 </div>
             </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function LibrarySong({ song }) {
+function LibrarySong({ song, songs, setCurrentSong }) {
+    function songSelect() {
+        console.log("single song --->", song);
+        setCurrentSong(song);
+    }
+
     return (
         <div>
-            <div className="library-song">
+            <div className="library-song" onClick={songSelect}>
                 <img src={song.cover} alt={song.name} />
                 <div className="song-description">
                     <h3>{song.name}</h3>
